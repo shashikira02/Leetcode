9045cba48ce11a3d23e0c26d8b361eaf68fc4b12
shashikira02 <125930477+shashikira02@users.noreply.github.com>
2025-01-13 18:16:29 +0530

    Time: 7 ms (95.48%), Space: 24.8 MB (99.75%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool canBeValid(string& s, string& locked) {
+        const int n=s.size();
+        if (n&1) return 0;
+        int bMin=0, bMax=0;
+        for(int i=0; i<n; i++){
+            bool op=s[i]=='(', wild=locked[i]=='0';
+            bMin+=(!op|wild)?-1:1;
+            bMax+=(op|wild)?1:-1;
+            if (bMax<0) return 0;
+            bMin=max(bMin, 0);
+        }
+        return bMin==0;
+    }
+};
