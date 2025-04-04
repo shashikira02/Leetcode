cb610710f0633abc0aba40cc56a12c9836b52e94
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-04-04 19:57:58 +0530

    Time: 21 ms (91.61%), Space: 23.6 MB (26.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
 
-    void dfs(int row, int col, vector<vector<int>> & vis, vector<vector<char>>& grid){
+    void bfs(int row, int col,vector<vector<char>>& grid){
 
-        vis[row][col] =1;
+        queue<pair<int,int>>q;
+        q.push({row, col});
+        grid[row][col] ='0';
         int n = grid.size(), m = grid[0].size();
 
         vector<int> dRow = {-1, 0, 1, 0};
         vector<int> dCol = {0, 1, 0, -1};
 
-            for (int i = 0; i < 4; i++) { 
-                int nRow = row + dRow[i];
-                int nCol = col + dCol[i];
+        while(!q.empty()){
+            int currRow = q.front().first, currCol = q.front().second;
+            q.pop();
 
-                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && !vis[nRow][nCol]) {
-                    dfs(nRow, nCol, vis, grid);
-                    
+            for (int i = 0; i < 4; i++) { 
+                int nRow = currRow + dRow[i];
+                int nCol = currCol + dCol[i];
+
+                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' ) {
+                    grid[nRow][nCol] = '0';
+                    q.push({nRow, nCol});
+                }
                 }
             }
         }
@@ -25,14 +32,13 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<vector<int>> vis(n, vector<int>(m,0));
         int count =0;
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] =='1'){
+                if(grid[i][j] =='1'){
                     count++;
-                    dfs(i,j,vis,grid);
+                    bfs(i,j,grid);
                 }
             }
         }
