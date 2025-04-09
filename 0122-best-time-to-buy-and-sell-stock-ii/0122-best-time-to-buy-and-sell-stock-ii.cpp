88693c7830a9dc1e830aa65cefa0383f5c3e89b4
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-04-09 23:18:52 +0530

    Time: 0 ms (100%), Space: 17.4 MB (30.3%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size(), maxProfit =0;
+        int n = prices.size();
+        vector<int>next(2,0), curr(2,0);
 
-        for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+        // DP base case: at the end of the array, profit is 0 regardless of
+        // bought/sell state
+        next[0] = 0;
+        next[1] = 0;
+
+        // Fill the DP table from bottom to top
+        for (int index = n - 1; index >= 0; index--) {
+            for (int bought = 0; bought <= 1; bought++) {
+                int skip = next[bought]; // Skip the current day
+                int profit = 0;
+
+                if (bought) {
+                    // If we have already bought, we can sell today
+                    int sell = prices[index] + next[0];
+                    profit = max(skip, sell);
+                } else {
+                    // If we haven't bought, we can buy today
+                    int buy = -prices[index] + next[1];
+                    profit = max(skip, buy);
+                }
+
+                // Store the calculated profit in the DP table
+                curr[bought] = profit;
             }
+            next = curr;
         }
-
-        return maxProfit;
+        return curr[0];
     }
 };
