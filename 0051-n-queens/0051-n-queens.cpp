54a3af87db20edb0e8d854610693d60b8cf971e7
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-04-13 00:23:22 +0530

    Time: 3 ms (65.14%), Space: 10.1 MB (92.69%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -3,17 +3,18 @@ public:
     bool isSafe(int row, int col, vector<string>& board, int n){
         int tempRow = row, tempCol = col;
 
-        while(row >= 0 && col>=0){
+        while(row >= 0 && col>=0){ // Check upper diagonal
             if(board[row][col] == 'Q')return false;
             row--, col--;
         }
+        // Check left column
         col = tempCol;
         row = tempRow;
         while(col >= 0){
             if(board[row][col] == 'Q')return false;
             col--;
         }
-
+        // Check lower diagonal
         col = tempCol;
         row = tempRow;
         while(row<n && col >= 0){
@@ -25,15 +26,16 @@ public:
 
     }
     void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n) {
-      if (col == n) {
+      if (col == n) {// All queens placed
         ans.push_back(board);
         return;
       }
+      // Try placing queen in each row
       for (int row = 0; row < n; row++) {
         if (isSafe(row, col, board,n)) {
-          board[row][col] = 'Q';
+          board[row][col] = 'Q';// Place queen
           solve(col + 1, board, ans, n);
-          board[row][col] = '.';          
+          board[row][col] = '.'; // Remove queen (backtrack)        
         }
       }
     }
@@ -41,10 +43,10 @@ public:
   public:
     vector<vector<string>> solveNQueens(int n) {
       vector<vector<string>> ans;
-      vector<string> board(n);
-      string s(n, '.');
+      vector<string> board(n); // empty board
+      string s(n, '.'); // Initialize empty row
       for (int i = 0; i < n; i++) {
-        board[i] = s;
+        board[i] = s;  // Fill board with empty rows
       }
       solve(0, board, ans, n);
       return ans;
