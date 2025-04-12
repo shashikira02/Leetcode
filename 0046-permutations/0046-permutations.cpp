041ae308f5b79378568e291317a7627246b9e21d
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-04-12 22:14:07 +0530

    Time: 0 ms (100%), Space: 11.1 MB (28.42%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    void permutations(vector<int>& nums, vector<int>& permutation,
-                      vector<vector<int>>& ans, vector<bool>& freq) {
-        if (permutation.size() == nums.size()) {
-            ans.push_back(permutation);
+    void permutations(int ind, vector<int>& nums,
+                      vector<vector<int>>& ans) {
+        if (ind == nums.size()) {
+            vector<int> permutation;
+            for(int i =0;i<nums.size();i++){
+                permutation.push_back(nums[i]);// current permutation.
+            }
+            ans.push_back(permutation); // Store the permutation in the result.
             return;
         }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (!freq[i]) {
-                freq[i] = true;
-                permutation.push_back(nums[i]);
-                permutations(nums, permutation, ans, freq);
-                permutation.pop_back();
-                freq[i] = false;
-            }
+        // Iterate through all possible elements for the current position.
+        for (int i = ind; i < nums.size(); i++) {
+           swap(i, ind, nums);// Swap to fix an element at the current position.
+           permutations(ind+1, nums, ans);
+           swap(i, ind, nums);// Backtrack to restore original order.
         }
+    }
+
+    void swap( int i , int j, vector<int>& nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> permutation;
-        vector<bool> freq(nums.size(), false);
-        permutations(nums, permutation, ans, freq);
+        permutations(0, nums, ans);
         return ans;
     }
 };
