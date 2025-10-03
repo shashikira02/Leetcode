2ddc58e66655a39aaaabdcdc1a3e94aa7497cf7f
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-10-04 05:19:56 +0530

    Time: 27 ms (66.7%), Space: 18 MB (71.28%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.cpp
@@ -1,38 +1,44 @@
 class Solution {
 public:
-    int trapRainWater(vector<vector<int>>& h) {
-    int row = h.size();
-    int col = h[0].size();
-    if(row<=2 or col<=2)    return 0;    
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    vector<vector<int>> vis(row, vector(col,0)) ;
-    
-    for(int i = 0;i<row; i++){
-        for(int j = 0; j<col; j++){
-            if(i==0 or j==0 or i ==row-1 or j== col-1){
-                pq.push({h[i][j], {i, j}});
-                vis[i][j] = 1;
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int row = heightMap.size(), col = heightMap[0].size();
+
+        if (row <= 2 or col <= 2)
+            return 0;
+
+        priority_queue<pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>, greater<>> pq;
+        vector<vector<int>> vis(row, vector(col, 0));
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || j == 0 || i == row - 1 || j == col - 1) {
+                    pq.push({heightMap[i][j], {i,j}});
+                    vis[i][j] =1;
+                }
             }
         }
-    }
-    vector<pair<int, int>> dir = {{-1, 0}, {1,0}, {0, -1}, {0, 1}};
-    int ans = 0;
-    while(!pq.empty()){
-        int x = pq.top().first;
-        int r = pq.top().second.first;
-        int c = pq.top().second.second;
-        pq.pop();
-        for(int i = 0; i<dir.size(); i++){
-            int nr = dir[i].first + r;
-            int nc = dir[i].second +c;            
-            if(nr>=0 and nc>=0 and nr<row and nc<col and vis[nr][nc]==0){
-                ans += max(0, x - h[nr][nc]);
-                vis[nr][nc]=1;
-                pq.push({max(h[nr][nc], x), {nr, nc}});
+
+        vector<pair<int,int>> dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int ans =0;
+
+        while(!pq.empty()){
+            int h = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+            pq.pop();
+
+            for(int i =0;i<4;i++){
+                int newR = dir[i].first+r;
+                int newC = dir[i].second+c;
+
+                if(newR >=0 && newC >=0 && newR<row && newC<col && vis[newR][newC]==0){
+                    ans += max(0,h-heightMap[newR][newC]);
+                    vis[newR][newC]=1;
+                    pq.push({max(heightMap[newR][newC], h), {newR, newC}});
+                }
             }
         }
-        
+        return ans;
     }
-    return ans;
-}
 };
