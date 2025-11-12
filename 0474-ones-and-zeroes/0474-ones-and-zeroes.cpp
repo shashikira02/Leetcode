4279589bc3a7e1f2d2e9b4ced1ceea90ea09ed53
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-11-12 20:52:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,25 +1,21 @@
 class Solution {
-    int findMaxi(vector<string>& strs, int m, int n, int ind, vector<vector<vector<int>>> &dp) {
+    int findMaxi(vector<string>& strs, int m, int n, int ind) {
         if (ind == strs.size())
             return 0;
-        if(dp[m][n][ind] !=-1){
-            return dp[m][n][ind];
-        }
 
         int zeros = count(strs[ind].begin(), strs[ind].end(), '0');
         int ones = strs[ind].size() - zeros;
 
         if (m >= zeros && n >= ones) {
-            return dp[m][n][ind] = max(1 + findMaxi(strs, m - zeros, n - ones, ind + 1, dp),
-                       findMaxi(strs, m, n, ind + 1, dp));
+            return max(1 + findMaxi(strs, m - zeros, n - ones, ind + 1),
+                       findMaxi(strs, m, n, ind + 1));
         }
 
-        return dp[m][n][ind]= findMaxi(strs,m,n, ind+1, dp);
+        return findMaxi(strs,m,n, ind+1);
     }
 
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<vector<int>>>dp(m+1, vector<vector<int>>(n+1,vector<int>(strs.size(), -1)));
-        return findMaxi(strs, m, n, 0, dp);
+        return findMaxi(strs, m, n, 0);
     }
 };
