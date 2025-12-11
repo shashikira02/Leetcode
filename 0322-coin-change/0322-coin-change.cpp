9d77434b4ec08edaf9b8b74384cfa3c4c99a1f37
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-11 12:02:05 +0530

    Time: 64 ms (10.21%), Space: 54 MB (8.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+    int totalCoins( int i,  vector<int>& coins, int amount, vector<vector<int>>&dp){
+        if(amount == 0)return 0;
+        if(amount< 0)return INT_MAX;
+        if(i >= coins.size())return INT_MAX;
+
+        if(dp[i][amount]!= -1)return dp[i][amount];
+        if(coins[i] == amount) return 1;
+        //take
+        int take = totalCoins(i, coins, amount - coins[i], dp);
+        if(take != INT_MAX)take +=1;
+
+        //not take
+        int notTake = totalCoins(i+1, coins, amount, dp);
+
+        return dp[i][amount]= min((int)take, (int)notTake);
+    }
+
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0) return 0;
-        int n = coins.size();
-        if (n == 0) {
-            if (amount % coins[0] == 0) return amount / coins[0];
-            return 1e7;
-        }
-        vector<int>curr(amount+1, 1e7), prev(amount+1, 1e7);
-        curr[0] = prev[0] = 0;
-
-        for (int ind = 0; ind < n; ind++) {
-            for (int mon = 1; mon <= amount; mon++) {
-                int skip = (ind == 0) ? 1e7 : prev[mon];
-                int take = 1e7;
-                if (mon >= coins[ind]) {
-                    take = 1 + curr[mon - coins[ind]];
-                }
-                curr[mon] = min(skip, take);
-            }
-            prev = curr;
-        }
-
-        return prev[amount] >= 1e7 ? -1 : prev[amount];
+        vector<vector<int>>dp(coins.size()+1, vector<int>(amount+1, -1));
+        int ans = totalCoins(0, coins, amount, dp) ;
+    
+        if(ans == INT_MAX)return -1;
+        else return ans;
     }
 };
