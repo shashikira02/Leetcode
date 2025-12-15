5a2f3539df00a792a3b20b7e7d4fc6256264e34b
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-16 02:31:14 +0530

    Time: 0 ms (100%), Space: 9.2 MB (74.14%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target){
+    int solve(vector<int>& nums, int target, vector<int>& dp){
         if(target == 0)return 1;
         if(target < 0)return 0;
-
-        int ways =0;
+        if(dp[target] != -1)return dp[target];
+        int ways = 0;
 
         for(auto num: nums){
-            ways +=solve(nums, target-num);
+            ways +=solve(nums, target-num, dp);
         }
 
-        return ways;
+        return dp[target] = ways;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        return solve(nums,target);
+        vector<int>dp(target+1, -1);
+        return solve(nums,target, dp);
     }
 };
