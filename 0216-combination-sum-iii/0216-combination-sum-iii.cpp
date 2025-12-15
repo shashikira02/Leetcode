82e534f5849f85a3bfb9fc1d912271a410638970
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-16 01:37:52 +0530

    Time: 0 ms (100%), Space: 9.8 MB (14.58%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    void func(int ind, int sum,int n,vector<vector<int>>& ans,vector<int>&list,int k){
-        if(sum == n && k ==0){
-            ans.push_back(list);
+    void func(int i, int rem, int total, vector<int> temp,
+              vector<vector<int>>& ans) {
+        if (rem == 0 && total == 0) {
+            ans.push_back((temp));
             return;
         }
-        if(ind>n)return;
-        for(int i =ind;i<=9;i++){
-            if(i>n)break;
-            list.push_back(i);
-            func(i+1,sum+i,n,ans,list,k-1);
-            list.pop_back();
-        }
+        if (i > 9 || total < 0 || rem < 0)
+            return;
 
+        for (int ind = i; ind <= 9; ind++) {
+            // take
+            temp.push_back(ind);
+            func(ind + 1, rem - 1, total - ind, temp, ans);
+            temp.pop_back();
+        }
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
-        vector<int> list;
-        func(1,0,n,ans,list,k);
+        vector<int> temp;
+
+        func(1, k, n, temp, ans);
         return ans;
     }
 };
