71d5d336097e189a109adf19c098c0d5cc639090
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-17 01:57:12 +0530

    Time: 52 ms (11.13%), Space: 24.9 MB (12.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-
-    void bfs(int row, int col,vector<vector<char>>& grid){
-
-        queue<pair<int,int>>q;
-        q.push({row, col});
-        grid[row][col] ='0';
+    void dfs(int row, int col, vector<vector<bool>>& vis,
+             vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
+                
+        vis[row][col] = true;
 
         vector<int> dRow = {-1, 0, 1, 0};
         vector<int> dCol = {0, 1, 0, -1};
+        for (int i = 0; i < 4; i++) {
+            int nRow = row + dRow[i];
+            int nCol = col + dCol[i];
 
-        while(!q.empty()){
-            int currRow = q.front().first, currCol = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++) { 
-                int nRow = currRow + dRow[i];
-                int nCol = currCol + dCol[i];
-
-                if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' ) {
-                    grid[nRow][nCol] = '0';
-                    q.push({nRow, nCol});
-                }
-                }
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                vis[nRow][nCol] == false && grid[nRow][nCol] == '1') {
+                    dfs(nRow, nCol, vis, grid);
             }
         }
+    }
 
-
-    
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
 
-        int count =0;
+        int count = 0;
 
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j] =='1'){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
                     count++;
-                    bfs(i,j,grid);
+                    dfs(i, j, vis, grid);
                 }
             }
         }
+
         return count;
     }
 };
-
