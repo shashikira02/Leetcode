abf68af74c1ac55eff7aa410d1e7c5767dbb4932
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-16 23:41:50 +0530

    Time: 663 ms (6.27%), Space: 55.8 MB (6.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    bool jump(int i, vector<int>& nums, vector<int>& dp) {
         int n = nums.size();
-        int maxi =0;
-        if(nums[0] == 0 && n ==1)return true;
-        for(int i =0;i<n;i++){
-            if(i > maxi)return false;
-            maxi = max(maxi, i + nums[i]);
+
+        if (i == n - 1 || i >= n)
+            return true;
+        if(dp[i]!=-1)return dp[i];
+
+        for (int ind = i + 1; ind <= i + nums[i] && i < n; ind++) {
+            if (jump(ind, nums, dp))
+                return dp[i]= 1;
         }
 
-        return maxi >= n-1;
+        return dp[i]= 0;
+    }
+
+    bool canJump(vector<int>& nums) {
+
+        vector<int> dp(nums.size(), -1);
+
+        return jump(0, nums, dp);
     }
 };
