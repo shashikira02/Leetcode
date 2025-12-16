631e3ec08a38d06c9532974a37f19379d722accb
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2025-12-16 07:12:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    bool canRob(vector<int>&nums, int mid, int k){
-        int count =0, n = nums.size();
-        for(int i =0; i<n;i++){
-            if(nums[i] <= mid){
-                count++;
-                i++;
-            }
-        }
-        return count >= k;
-    }
-    int minCapability(vector<int>& nums, int k) {
-        int l =1, r = *max_element(nums.begin(), nums.end()), ans =r;
+    int solve(int i, int k, vector<int>& nums, vector<vector<int>>& dp) {
+        if (k == 0)
+            return 0;
 
-        while(l <= r){
-            int mid = (l+r)/2;
-            if(canRob(nums, mid, k)){
-                ans = mid;
-                r = mid-1;
-            }
-            else {
-                l = mid+1;
-            }
-            
+        if (i >= nums.size()) {
+            return  INT_MAX;
         }
-        return ans;
+        
+        if(dp[i][k] != -1)return dp[i][k];
+
+        int take = max(nums[i], solve(i + 2, k - 1, nums, dp));
+
+        int skip = solve(i + 1, k, nums, dp);
+
+        return dp[i][k] = min(take, skip);
+    }
+
+    int minCapability(vector<int>& nums, int k) { 
+        vector<vector<int>>dp(nums.size()+1, vector<int>(k+1, -1));
+        return solve(0, k, nums, dp); 
     }
 };
