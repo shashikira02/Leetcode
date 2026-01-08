28e6c35ed5aa5581b6462242e2132d982d4674eb
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2026-01-08 08:26:50 +0530

    Time: 43 ms (21.6%), Space: 18.9 MB (23.2%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int maxi = -1e9;
-    int func(int i, int j, vector<int>& a, vector<int>& b){
+    int func(int i, int j, vector<int>& a, vector<int>& b, vector<vector<int>>& dp){
         if(i == a.size() || j == b.size())return maxi;
 
-        long long take = a[i]*b[j];
-        long long cont = a[i]* b[j] + func(i+1, j+1, a, b);
-        long long skipA = func(i+1, j, a, b);
-        long long skipB = func(i, j+1, a, b);
+        if(dp[i][j] != INT_MIN)return dp[i][j];
+        int take = a[i]*b[j];
+        int cont = a[i]* b[j] + func(i+1, j+1, a, b, dp);
+        int skipA = func(i+1, j, a, b, dp);
+        int skipB = func(i, j+1, a, b, dp);
 
-        return max({(int)take, (int)cont , (int)skipA, (int)skipB});
+        return dp[i][j] = max({take, cont , skipA, skipB});
     }
 
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
 
-        return func(0,0,nums1, nums2);
+        vector<vector<int>>dp(n, vector<int>(m, INT_MIN));
+
+        return func(0,0,nums1, nums2, dp);
 
     }
 };
