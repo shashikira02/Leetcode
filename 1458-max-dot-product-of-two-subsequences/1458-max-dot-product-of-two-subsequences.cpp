bbce3c2d9cec0c0ec41b4d13877c724ff2809a12
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2026-01-08 08:30:58 +0530

    Time: 11 ms (88.4%), Space: 17.9 MB (41.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int maxi = -1e9;
-    int func(int i, int j, vector<int>& a, vector<int>& b, vector<vector<int>>& dp){
-        if(i == a.size() || j == b.size())return maxi;
-
-        if(dp[i][j] != INT_MIN)return dp[i][j];
-        int take = a[i]*b[j];
-        int cont = a[i]* b[j] + func(i+1, j+1, a, b, dp);
-        int skipA = func(i+1, j, a, b, dp);
-        int skipB = func(i, j+1, a, b, dp);
-
-        return dp[i][j] = max({take, cont , skipA, skipB});
-    }
-
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
 
-        vector<vector<int>>dp(n, vector<int>(m, INT_MIN));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1e9));
 
-        return func(0,0,nums1, nums2, dp);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int prod = nums1[i - 1] * nums2[j - 1];
 
+                dp[i][j] = max({prod, prod + dp[i - 1][j - 1], dp[i - 1][j],
+                                dp[i][j - 1]});
+            }
+        }
+
+        return dp[n][m];
     }
 };
