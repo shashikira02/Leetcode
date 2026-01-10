fa47e97cc9bcd1dc02ee12c995e4330174a175f1
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2026-01-11 03:47:56 +0530

    Time: 15 ms (72.63%), Space: 18.2 MB (48.06%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,51 +1,25 @@
 class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
-        
-        // Make sure s2 is smaller string
-        if (s1.length() < s2.length()) {
-            return minimumDeleteSum(s2, s1);
-        }
-        
-        // Case for empty s1
-        int m = s1.length(), n = s2.length();
-        vector<int> currRow(n + 1);
-        for (int j = 1; j <= n; j++) {
-            currRow[j] = currRow[j - 1] + s2[j - 1];
-        }
-        
-        // Compute answer row-by-row
-        for (int i = 1; i <= m; i++) {
-            
-            int diag = currRow[0];
-            currRow[0] += s1[i - 1];
-            
-            for (int j = 1; j <= n; j++) {
+        vector<vector<int>> dp(s1.size() + 1, vector<int>(s2.size() + 1, 0));
+        int n = s1.size(), m = s2.size();
 
-                int answer;
-                
-                // If characters are the same, the answer is top-left-diagonal value
-                if (s1[i - 1] == s2[j - 1]) {
-                    answer = diag;
-                }
-                
-                // Otherwise, the answer is minimum of top and left values with
-                // deleted character's ASCII value
-                else {
-                    answer = min(
-                        s1[i - 1] + currRow[j],
-                        s2[j - 1] + currRow[j - 1]
-                    );
-                }
-                
-                // Before overwriting currRow[j] with answer, save it in diag
-                // for the next column
-                diag = currRow[j];
-                currRow[j] = answer;
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = dp[i - 1][0] + s1[i - 1];
+
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = dp[0][j - 1] + s2[j - 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] =
+                        min(s1[i - 1] + dp[i - 1][j], s2[j - 1] + dp[i][j - 1]);
             }
         }
-        
-        // Return the answer
-        return currRow[n];
+
+        return dp[n][m];
     }
 };
