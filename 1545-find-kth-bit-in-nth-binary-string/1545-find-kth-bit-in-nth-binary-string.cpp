35e200c01450516fe90e5e27fd87c2578aedb4b6
Shashi Kiran Perka <125930477+shashikira02@users.noreply.github.com>
2026-03-04 00:07:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        // Find the position of the rightmost set bit in k
-        // This helps determine which "section" of the string we're in
+
         int positionInSection = k & -k;
 
-        // Determine if k is in the inverted part of the string
-        // This checks if the bit to the left of the rightmost set bit is 1
         bool isInInvertedPart = ((k / positionInSection) >> 1 & 1) == 1;
 
         bool originalBitIsOne = (k & 1) == 0;
